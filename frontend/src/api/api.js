da66dc104ed15139df9258ec19dddf2377fe4203
Atumah Daniel <atumahdaniel@gmail.com>
2021-09-20 16:49:42 +0100

    mixed content error

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://memories-dancy.herokuapp.com/" });
+const API = axios.create({ baseURL: "https://memories-dancy.herokuapp.com" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
